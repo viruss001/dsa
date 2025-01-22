7413c21d8bb629a3bf60c3be9288a45e38218045
Suraj TIwari <122778544+viruss001@users.noreply.github.com>
2025-01-22 14:17:25 +0530

    Update 6verticalorder.py

--- Tree/binary tree/6verticalorder.py
+++ Tree/binary tree/6verticalorder.py
@@ -5,42 +5,33 @@ root=Creating(root)
 
 
 
-def vertical(root,h,v):
-    if root is None :
+def vertical(root, h=0, v=0):
+    if root is None:
         return {}
     
-    if root.left is None and root.right is None:
-        return {h:[{v:[root.data]}]}
-   
-    left=vertical(root.left,h-1,v+1)
-    right=vertical(root.right,h+1,v+1)
-    ans={**left,**right}
-    keylist=list(ans.keys())
-    if h in keylist:
-    #    when it is on same level then it create a problem
-        keyss=[]
-        for i in ans[h]:
-            print(i,"i")
-            keyss.extend(i.keys())
-        print(keyss,"keyss" , v)
-        print(v in keyss,"v in keyss")
-        if v in keyss:
-            print(v in keyss,"v in keyss")
-            print(keyss,"keyss")
-            for j in ans[h]:
-
-                if v in j.keys():
-                    print(v in j.keys())
-                    j[v].append(root.data)
-
-
-        else:
-            ans[h].append({v:[root.data]})
-    else:
-      
-        temp={h:[{v:[root.data]}]}
-        ans={**ans,**temp}
+    # Recursive calls for left and right children
+    left = vertical(root.left, h - 1, v + 1)
+    right = vertical(root.right, h + 1, v + 1)
+    
+    # Combine left and right results
+    ans = {**left, **right}
+    
+    # Ensure the current horizontal distance `h` exists in the dictionary
+    if h not in ans:
+        ans[h] = []
+    
+    # Check if there is an entry for the current vertical level `v` in `ans[h]`
+    found = False
+    for entry in ans[h]:
+        if v in entry:
+            entry[v].append(root.data)
+            found = True
+            break
+    
+    # If no entry for `v` exists, create a new one
+    if not found:
+        ans[h].append({v: [root.data]})
+    
     return ans
-
 a =vertical(root,0,1)
 print(a)
